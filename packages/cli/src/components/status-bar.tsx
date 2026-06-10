28ec1ec1475bd23c5dd5c027515693f2f918f4a7
Nicolae Moraru <morarunicolae96@gmail.com>
2026-06-10 17:20:40 +0200

    01 project setup and component architecture

---
+++ packages/cli/src/components/status-bar.tsx
@@ -0,0 +1,14 @@
+import { TextAttributes } from "@opentui/core";
+
+export function StatusBar(){
+
+    return(
+        <box flexDirection="row" gap={1}>
+            <text fg="cyan">Build</text>
+            <text attributes={TextAttributes.DIM} fg="gray">
+                &#8250;
+            </text>
+            <text>best brain ever ahah</text>
+        </box>
+    )
+}
